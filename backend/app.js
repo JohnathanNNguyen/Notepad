b01eb60324cd57c919be0bd2eb6eb67e8aef2784
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-15 22:31:05 -0800

    removed a post

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods',
-   'GET, POST, PATCH, DELETE, OPTIONS')
-  next()
-})
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
-app.use( '/api/posts',(req, res, next)=>{
+app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
-      id: 'laskjdca',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
+      id: "laskjdca",
+      title: "First server-side post",
+      content: "This is coming from the server",
     },
-    {
-      id: 'lkjhgads',
-      title: 'Second server-side post',
-      content: 'This is also coming from the server'
-    },
-  ]
+  ];
   res.status(200).json({
-    message: 'Post fetched succesfully!',
-    posts: posts
+    message: "Post fetched succesfully!",
+    posts: posts,
   });
 });
 
